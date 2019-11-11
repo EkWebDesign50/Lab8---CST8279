ca64c67b575ead42b4e4f1cfb7af93e67e51a935
EkWebDesign50 <54911050+EkWebDesign50@users.noreply.github.com>
2019-11-10 20:34:01 -0500

    Add files via upload

---
+++ Lab8 tasks.py
@@ -0,0 +1,33 @@
+def ConvertToCsv(fileName):
+    f = open(fileName, "r")
+    lines = f.readlines()
+    f.close()
+    f = open(fileName[0:len(fileName)-4]+".csv", "w")
+    f.write("\"First Name\",\"Count\"\n")
+    for lino in lines:
+        spaceIndex = lino.find(" ")
+        newline = "\"" + lino[0:spaceIndex] + "\","+lino[spaceIndex+1:len(lino)]
+        f.write(newline)
+    f.close()
+
+# ConvertToCsv("2000_GirlsNames.txt")
+# ConvertToCsv("2000_BoysNames.txt")
+
+def displayLines():
+    fileName = input("Enter the name of a csv file: ")
+    f = open(fileName, "r")
+    lineList = f.readlines()
+    print(lineList)
+
+displayLines()
+
+
+def loadDict(fileName):
+    f = open(fileName, "r")
+    lines = f.readlines()
+    f.close()
+    d = {}
+    for lin in lines:
+        a,b = lin.split(",")
+        d[a]=b
+    return d
